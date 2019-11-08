217aed256f191863b37bfea29d57dd8d2a35310f
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2019-11-07 20:01:27 -0800

    Changed formatting

--- src/components/Display.js
+++ src/components/Display.js
@@ -5,11 +5,15 @@ function Display (props) {
 
     return (
         <div className="display-container">
+            <div className="ball-counter">
             <h2>Balls</h2>
             <h4>{props.balls}</h4>
+            </div>
 
+            <div className="strike-counter">
             <h2>Strikes</h2>
             <h4>{props.strikes}</h4>
+            </div>
         </div>
     );
 };
